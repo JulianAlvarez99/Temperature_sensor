a184ecdca61ee38229c063e0afada832fb579b53
Julian Alvarez <131938114+JulianAlvarez99@users.noreply.github.com>
2023-06-02 14:34:34 -0300

    File changed, print functions added

--- temperature_sensor.c
+++ temperature_sensor.c
@@ -4,24 +4,25 @@
 #include "temperature_sensor.h"
 #include "stack_static.h"
 
-stack* stack_set_temperature()
+void stack_set_temperature(stack** sensor)
 {
     int measures = 1440;
 
     reading* sample = (reading*)malloc(measures*sizeof(reading));
 
-    stack* sensor = stack_new(measures);
+    //stack* sensor = stack_new(measures);
 
     srand(time(NULL));
     while( measures > 0 )
     {
         sample->minute = rand()%1441;
+        //printf("minuto %d ", sample->minute);
         sample->temperature = rand()%41-10; // minimo -10 grados valor maximo 40 grados
-        stack_push(sensor,sample);
+        //printf("temperatura %d \n", sample->temperature);
+        stack_push(*sensor,sample);
+        //stack_print(*sensor);
         measures--;
     }
-
-    return sensor;
 }
 
 
